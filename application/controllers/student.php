92091ca019917abaabc7c7cef5c549a4c620c646
Himanshu Goswami <goswami.himanshu1807@gmail.com>
2019-03-27 14:17:19 +0530

    files added

---
+++ application/controllers/student.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Student extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('system_admin_dashboard');
+	}
+}
---
+++ application/views/global/global_header_view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Project</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+
+	
+	
